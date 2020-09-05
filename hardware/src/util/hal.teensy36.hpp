9696c8b9e16cd1761753e3c38e01760bd13125bb
ldursw <37294448+ldursw@users.noreply.github.com>
2020-09-05 15:46:47 -0300

    Move HAL to one file per platform

---
+++ hardware/src/util/hal.teensy36.hpp
@@ -0,0 +1,10 @@
+#define SdFatCls SdFatSdio
+#define SerialUSB Serial
+#define PSG_WE 29
+#define PSG_DAC A21
+#define PSG_ATTENUATION 6
+#define FM_IC 28
+#define FM_WR 27
+#define FM_A0 26
+#define FM_A1 25
+#define enableDAC0()
